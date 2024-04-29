5407c85dac05bd75cb79589c8526076a7f1649a0
m-murad <murad.kuka@gmail.com>
2024-04-29 13:09:22 +0200

    add http client package
    Signed-off-by: m-murad <murad.kuka@gmail.com>

---
+++ http/client.go
@@ -0,0 +1,33 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/m-murad/go-common/metrics"
+)
+
+type Client struct {
+	cli         *http.Client
+	metrics     metrics.Service
+	serviceName string
+
+	// client level tags
+	tags []string
+}
+
+func NewClient(httpCli *http.Client, metrics metrics.Service, serviceName string) *Client {
+	cli := Client{
+		cli:         httpCli,
+		metrics:     metrics,
+		serviceName: serviceName,
+		tags:        []string{"service:" + serviceName},
+	}
+
+	return &cli
+}
+
+func (c *Client) Do(req *http.Request) (*http.Response, error) {
+	rsp, err := c.cli.Do(req)
+	c.metrics.Incr("http_request", 1, []string{"status:" + rsp.Status, "method:" + req.Method, "path:" + req.URL.Path})
+	return rsp, err
+}
---
+++ http/go.mod
@@ -0,0 +1,15 @@
+module github.com/m-murad/go-common/http
+
+go 1.22.1
+
+require github.com/m-murad/go-common/metrics v0.0.0-20240429100343-ad321d8d2db3
+
+require (
+	github.com/DataDog/datadog-go v4.8.3+incompatible // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
